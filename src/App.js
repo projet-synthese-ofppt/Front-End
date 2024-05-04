4a60a83907d4c2bff30079c9909452609c0fff01
soukaina513 <souka.zirmme@gmail.com>
2024-05-04 19:49:15 +0100

    modification de soukaina

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Dashbord2 from "./components/Admin/dashbord2";
+import { Route,Routes } from "react-router-dom";
+import GestionnaireLogin from "./components/Gestionnaire/GestionnaireLogin";
+import Creation_Formation from "./components/Gestionnaire/Creation_Formation";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+
+    <Route path="/dashboard" element={<Dashbord2/>}></Route>
+    
+    <Route path="/Creation_Formation" element={<Creation_Formation/>}></Route>
+    <Route path="/GestionnaireLogin" element={<GestionnaireLogin/>}></Route>
+    </Routes>
+    
   );
 }
 

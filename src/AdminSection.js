3fd6a81e64cb123223a62005cf94cb3755948b2e
ImaneKhibi2003 <khibiimane@gmail.com>
2024-05-12 17:42:13 +0100

    profil admin imane

--- src/AdminSection.js
+++ src/AdminSection.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
-import Dashbord2 from './components/admin/dashbord2';
+import Dashbord2 from './components/Admin/dashbord2';
 import Sidebar from './Sidebar';
-import LoginAdmin from './components/admin/LoginAdmin';
-import ListGestionnaire from './components/admin/ListGestionnaire';
-import ListFormateur from './components/admin/listFormateur';
-import ListFormation from './components/admin/ListFormation';
-import Ajoutergestionnaire from './components/admin/addGestionnaire';
+import LoginAdmin from './components/Admin/LoginAdmin';
+import ListGestionnaire from './components/Admin/ListGestionnaire';
+import ListFormateur from './components/Admin/listFormateur';
+import ListFormation from './components/Admin/ListFormation';
+import Ajoutergestionnaire from './components/Admin/addGestionnaire';
 import ProfileGestionnaire from './components/Gestionnaire/ProfileGestionnaire';
 import History from './History';
+import Profile from './components/Admin/Profile';
 
 function AdminSection() {
   return<>
   <Sidebar/>
   <Routes>
     <Route path="/dashboard" element={<Dashbord2/>}></Route>
+    <Route path='/admin/profile' element={<Profile/>}></Route>
     <Route path="/manager" element={<ListGestionnaire/>}></Route>
     <Route path="/trainers" element={<ListFormateur/>}></Route>
     <Route path="/training" element={<ListFormation/>}></Route>

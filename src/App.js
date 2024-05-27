83926e5f7e940793b6a0b1664530b5561a66f7c2
soukaina513 <souka.zirmme@gmail.com>
2024-05-27 18:34:55 +0100

    c

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import GestionnaireLogin from "./components/Gestionnaire/GestionnaireLogin";
 import GestionnaireSection from "./GestionnaireSection";
+import AdminSection from "./AdminSection";
+import LoginAdmin from "./components/Admin/LoginAdmin";
 
 function App() {
 
-  
-    
-    
-    
-   
-    
-    
-    
-    
 
   return (
     <div className="SiteContainer">
@@ -22,11 +15,14 @@ function App() {
         <Route
           path="/GestionnaireLogin"
           element={<GestionnaireLogin />}
-        ></Route>
+        >
+
+        </Route>
         <Route path="*" element={<GestionnaireSection />}></Route>
-        <Route path="/Login" element={<LoginAdmin/>}></Route>
-    <Route path='*' element={<AdminSection/>} ></Route>
-      </Routes>
+        {/* <Route path="/Login" element={<LoginAdmin/>}></Route>
+         <Route path='*' element={<AdminSection/>} ></Route>
+*/}
+      </Routes> 
 
 
       {/* <Routes>

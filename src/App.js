ad5d0a0c848371fb4c7b6811f5fe2213ef92762a
Noureddine-Elkhalifa <mnour.elkhalifa@gmail.com>
2024-06-02 16:09:09 +0100

    c

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GestionnaireLogin from "./components/Gestionnaire/GestionnaireLogin";
 import GestionnaireSection from "./GestionnaireSection";
 import AdminSection from "./AdminSection";
 import LoginAdmin from "./components/Admin/LoginAdmin";
+import ProfilFormateur from "./components/Gestionnaire/profilFormateur";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/GestionnaireLogin" element={<GestionnaireLogin />}></Route>
         <Route path="*" element={<GestionnaireSection />}></Route>
         {/* <Route path='*' element={<AdminSection/>}></Route> */}
+
+        <Route path="/profilFormateur/:id" element={<ProfilFormateur/>}></Route>
     </Routes>
     </div>
   );
